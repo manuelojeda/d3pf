c182a2ee9fa2ccedbbcf89b730a67523340194b8
Manuel Ojeda <manuelojedasistemas@gmail.com>
2020-03-28 15:21:24 -0700

    Vue Router

---
+++ src/views/Error/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>
+      Error view
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'ErrorView'
+})
+</script>
---
+++ src/views/Hero/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>
+      Hero view
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'HeroView'
+})
+</script>
